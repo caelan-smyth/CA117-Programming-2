74ae5021c583a28add8bbc2ae69bfc1ba3bdff59
Caelan <infamouspot@gmail.com>
2019-02-20 18:14:00 +0000

    oof

--- Week-1/chop_011.py
+++ Week-1/chop_011.py
@@ -1,11 +1,13 @@
 def chop(s):
     if len(s) > 2:
-        return s[1:-1]
+        return s[1:len(s)-1]
 
 def main():
     import sys
     for line in sys.stdin:
-        print(chop(line.strip()))
+        cs = chop(line.strip())
+        if cs:
+            print(cs)
 
 if __name__ == '__main__':
     main()
